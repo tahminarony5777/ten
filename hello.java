44953df2dc4d08e2ef6d38e90bc68d6ad626b10f
Tahmina Rony <tahminarony5777@gmail.com>
2023-11-16 13:44:25 +0600

    Added a new method call display hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 pulic class Hello{
     public static void main(String[] args){
         System.out.println('VS code is awesome');
+        greeting('Rony');
+    }
+    public static void display(String name){
+        System.out.println('welcome', name)
     }
 }
